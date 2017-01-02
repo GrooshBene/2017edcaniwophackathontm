7c78f63b94146aade844dc06efbf44943ede8a05
GrooshBene <wltn000129@gmail.com>
2017-01-02 10:26:39 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/**
+ * Created by GrooshBene on 2017-01-01.
+ */
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('compress', function() {
+    return gulp.src('jquery.final-countdown.js')
+        .pipe(uglify())
+        .pipe(rename('jquery.final-countdown.min.js'))
+        .pipe(gulp.dest('.'));
+});
